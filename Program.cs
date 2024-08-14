2700ad1b9a230cbae75b7b2dc4b005761df7d035
Marco Aurélio <165288580+marcodevweb@users.noreply.github.com>
2024-08-14 10:58:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿int x =10;
+double pi = 3.1415;
+char letra= "M"
+bool istrue = true;
+string texto= "uma mensagem"
+
+double outrovalor;
+
+outrovalor=x;
+
+int valorinteiro= (int) pi;
+
+int y=Convert.ToInt32(texto);
+
+double numero;
+double numero2;
+double resultado;
+Console.WriteLine("digite um numero");
+numero=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("digite o segundo numero");
+numero2=Convert.ToInt32(Console.ReadLine());
+
+resultado=numero/numero2;
+
+Console.WriteLine("resultado");
+
+double soma=numero+numero2;
+double multiplicacao=numero*numero2;
+double subtracao=numero-numero2;
+Console.WriteLine(resultado);
+Console.WriteLine($"o valor da soma é: {soma}");
+Console.WriteLine("o valor da subtracao é:"+subtracao);
+
+
+
+
+
+
+
